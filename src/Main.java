4bbba2359a685acb5fa2fd392fd7d324e3e0299f
Fazilabi <fazilag@code.edu.az>
2023-02-28 08:12:16 +0400

    modified

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner scan1 = new Scanner(System.in);
-        System.out.print("ust deyeri daxil et : ");
-        int v1 = scan1.nextInt();
-        Scanner scan2 = new Scanner(System.in);
-        System.out.print("alt deyeri daxil et : ");
-        int v2 = scan2.nextInt();
-        int result = 1;
-
-        for (int i = 0; i < v1; i++) {
-            result *= v2;
+    static int recursiveMetod(int taban, int ust){
+        if (ust == 0){
+            return 1;
+        } else if (ust == 1 ) {
+            return taban;
         }
-        System.out.print("Netice: " + result);
+        return taban * recursiveMetod(taban, (ust-1));
+    }
+    public static void main(String[] args) {
+        Scanner scan1 = new Scanner(System.in);
+
+        System.out.print("ust deyeri daxil et : ");
+        int ust = scan1.nextInt();
+
+        System.out.print("alt deyeri daxil et : ");
+        int taban = scan1.nextInt();
+        System.out.println(recursiveMetod(taban, ust));
     }
 }
